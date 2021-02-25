ca3bd25c87e940a9d7be680041f4a6738cad6023
zzuwenjie <wen20121724@163.com>
2021-02-25 10:51:45 +0800

    peoperties

--- eureka-server/src/main/java/com/orange/Application.java
+++ eureka-server/src/main/java/com/orange/Application.java
@@ -1,5 +1,6 @@
 package com.orange;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class Application {
     public static void main(String[] args) {
+        // SpringApplication.run(Application.class, args);
         new SpringApplicationBuilder(Application.class).web(WebApplicationType.SERVLET).run(args);
     }
 }
